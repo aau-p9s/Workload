40d0c547cd92192b8aa04347f097ea120535f866
Thomas Jensen <mast3r@skade.dev>
2025-06-30 14:25:29 +0200

    fix

--- generator/lib/increasing_load_shape.py
+++ generator/lib/increasing_load_shape.py
@@ -8,10 +8,7 @@ def increasing_load_shape(_load_shape: Callable) -> Callable:
     def load_shape() -> int:
         raw = _load_shape()
         today = datetime.now().date()
-        scalar = (1 + (today - start_date).days) * 10
-        if scalar > 100:
-            return raw
-        else:
-            return raw * (scalar / 100)
+        scalar = (1 + (today - start_date).days)
+        return raw * scalar
 
     return load_shape
